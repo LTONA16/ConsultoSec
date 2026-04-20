b914dbc6cc148bcc673bdc85dc4b94a6f2362e5b
Luis Tona <luisftona@gmail.com>
2026-04-20 15:32:37 -0700

    logica del checklist creada, cambios a la bd, etc

--- ConsultoSec/backend/consultas/urls.py
+++ ConsultoSec/backend/consultas/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConsultaViewSet
+from .views import ConsultaViewSet, ChecklistItemViewSet, AreaCatalogoViewSet, RequisitoCatalogoViewSet
 
 # El router crea automáticamente las URLs para nuestro ViewSet
 router = DefaultRouter()
+router.register(r'areas-laboratorio', AreaCatalogoViewSet)
+router.register(r'requisitos-laboratorio', RequisitoCatalogoViewSet)
 router.register(r'consultas', ConsultaViewSet)
+router.register(r'checklists', ChecklistItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
