756d22fac62cb036e61dbf337da05e660320c7b4
Luis Tona <luisftona@gmail.com>
2026-05-05 22:30:02 -0700

    se actualizo el entrypoint.sh

--- ConsultoSec/backend/entrypoint.sh
+++ ConsultoSec/backend/entrypoint.sh
@@ -6,5 +6,22 @@ python manage.py migrate --noinput
 echo "Recopilando archivos estáticos..."
 python manage.py collectstatic --noinput
 
+# Crea el superusuario solo si no existe
+echo "Creando superusuario si no existe..."
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser(
+        username='$DJANGO_SUPERUSER_USERNAME',
+        email='$DJANGO_SUPERUSER_EMAIL',
+        password='$DJANGO_SUPERUSER_PASSWORD',
+        role='ADMIN'
+    )
+    print("Superusuario creado.")
+else:
+    print("Superusuario ya existe.")
+EOF
+
 echo "Iniciando Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
